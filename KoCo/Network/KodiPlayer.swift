65f878b8e20537700d0a1c71811ad3dbf460e356
dietWall <dietrich7@gmx.de>
2019-05-19 16:31:45 +0200

    Created media player base

---
+++ KoCo/Network/KodiPlayer.swift
@@ -0,0 +1,23 @@
+//
+//  KodiPlayer.swift
+//  KoCo
+//
+//  Created by admin on 19.05.19.
+//  Copyright © 2019 TH Rosenheim. All rights reserved.
+//
+
+import Foundation
+
+class KodiPlayer : Codable{
+    
+    let url : String
+    
+    let name : String
+    
+    let user : String?
+    
+    let password : String?
+    
+    
+    
+}
